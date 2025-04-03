ac89187767f3f2a035209a92f6d4f2d779ed502f
MateoSB1 <mateobrancato26@gmail.com>
2025-04-03 14:57:50 -0300

    Entrega para el Proyecto Final

---
+++ src/producto/forms.py
@@ -0,0 +1,13 @@
+from django import forms
+
+from . import models
+
+class CategoriaForm(forms.ModelForm):
+    class Meta:
+        model = models.Categoria
+        fields = "__all__"
+
+class ProductoForm(forms.ModelForm):
+    class Meta:
+        model = models.Producto
+        fields = "__all__"
---
+++ src/producto/views.py
@@ -0,0 +1,95 @@
+from django.shortcuts import redirect, render
+from django.urls import reverse_lazy
+from django.views.generic import CreateView, DeleteView, DetailView, ListView, UpdateView
+from django.contrib.auth.decorators import login_required, method_decorator
+from django.contrib.auth.mixins import LoginRequiredMixin
+
+from . import forms, models
+
+def index(request):
+    return render(request, "producto/index.html")
+
+
+# CATEGORIA - LISTVIEW
+def categoria_list(request):
+    busqueda = request.GET.get("busqueda")
+    if busqueda:
+        queryset = models.Categoria.objects.filter(nombre__icontains=busqueda)
+    else:
+        queryset = models.Categoria.objects.all()
+    context = {"object_list": queryset}
+    return render(request, "producto/categoria_list.html", context)
+
+
+# CATEGORIA - CREATEVIEW
+@login_required(login_url=reverse_lazy("core:login"))
+def categoria_create(request):
+    if request.method == "GET":
+        form = forms.CategoriaForm()
+    if request.method == "POST":
+        form = forms.CategoriaForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("producto:categoria_list")
+    return render(request, "producto/categoria_form.html", {"form": form})
+
+
+# CATEGORIA - UPDATEVIEW
+@login_required(login_url=reverse_lazy("core:login"))
+def categoria_update(request, pk: int):
+    query = models.Categoria.objects.get(id=pk)
+    if request.method == "GET":
+        form = forms.CategoriaForm(instance=query)
+    if request.method == "POST":
+        form = forms.CategoriaForm(request.POST, instance=query)
+        if form.is_valid():
+            form.save()
+            return redirect("producto:categoria_list")
+    return render(request, "producto/categoria_form.html", {"form": form})
+
+
+# CATEGORIA - DELETEVIEW
+@login_required(login_url=reverse_lazy("core:login"))
+def categoria_delete(request, pk: int):
+    query = models.Categoria.objects.get(id=pk)
+    if request.method == "POST":
+        query.delete()
+        return redirect("producto:categoria_list")
+    return render(request, "producto/categoria_confirm_delete.html", {"object": query})
+
+
+# PRODUCTO
+
+
+class ProductoListView(ListView):
+    model = models.Producto
+
+    def get_queryset(self):
+        busqueda = self.request.GET.get("busqueda")
+        if busqueda:
+            queryset = models.Producto.objects.filter(nombre__icontains=busqueda)
+        else:
+            queryset = models.Producto.objects.all()
+        return queryset
+
+class ProductoCreateView(LoginRequiredMixin, CreateView):
+    model = models.Producto
+    form_class = forms.ProductoForm
+    success_url = reverse_lazy("producto:producto_list")
+    login_url = reverse_lazy("core:login")
+
+class ProductoUpdateView(LoginRequiredMixin, UpdateView):
+    model = models.Producto
+    form_class = forms.ProductoForm
+    success_url = reverse_lazy("producto:producto_list")
+    login_url = reverse_lazy("core:login")
+
+
+class ProductoDetailView(DetailView):
+    model = models.Producto
+
+
+class ProductoDeleteView(DeleteView):
+    model = models.Producto
+    success_url = reverse_lazy("producto:producto_list")
+    login_url = reverse_lazy("core:login")
